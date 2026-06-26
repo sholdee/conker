2ae13caf09ea7e732263d86658e7d9167a342c1e
Ethan Shold <sholdee@gmail.com>
2026-06-26 11:20:08 -0500

    type: name 0 fields of struct_EA500 (name-only, ROM-identical) [typing]

--- conker/src/game_117490.c
+++ conker/src/game_117490.c
@@ -244,21 +244,21 @@ void func_150EA11C(Vec3F_EA11C *arg0, Vec3F_EA11C *arg1, s32 arg2, s32 arg3, f32
 typedef struct {
     /* 0x00 */ s32 unk0;
     /* 0x04 */ s32 unk4;
-    /* 0x08 */ u8  unk8;
+    /* 0x08 */ u8  ownerActorId;
     /* 0x09 */ u8  pad9[0x3];
-    /* 0x0C */ void *unkC;
-    /* 0x10 */ u8  unk10;
+    /* 0x0C */ void *ownerActor;
+    /* 0x10 */ u8  ownerTransformIndex;
     /* 0x11 */ u8  pad11[0x3];
-    /* 0x14 */ f32 unk14;
-    /* 0x18 */ f32 unk18;
-    /* 0x1C */ f32 unk1C;
-    /* 0x20 */ f32 unk20;
-    /* 0x24 */ f32 unk24;
-    /* 0x28 */ s16 unk28;
-    /* 0x2A */ u8  unk2A;
-    /* 0x2B */ u8  unk2B;
-    /* 0x2C */ s8  unk2C;
-    /* 0x2D */ u8  unk2D;
+    /* 0x14 */ f32 localOffsetX;
+    /* 0x18 */ f32 localOffsetY;
+    /* 0x1C */ f32 localOffsetZ;
+    /* 0x20 */ f32 spawnRadius;
+    /* 0x24 */ f32 spawnRate;
+    /* 0x28 */ s16 lifetime;
+    /* 0x2A */ u8  flags;
+    /* 0x2B */ u8  spawnCallbackIndex;
+    /* 0x2C */ s8  aliveCheckIndex;
+    /* 0x2D */ u8  behaviorIndex;
 } struct_EA500;
 
 extern f32 D_800A1438;
@@ -269,23 +269,23 @@ void *func_150EA500(void *arg0, s16 arg1, s32 arg2, s32 arg3) {
 
     sp20.unk0 = 0;
     sp20.unk4 = 0;
-    sp20.unk8 = *((u8 *)arg0 + 0x3B);
-    sp20.unkC = arg0;
-    sp20.unk10 = 1;
-    sp20.unk14 = 0.0f;
-    sp20.unk18 = 0.0f;
-    sp20.unk1C = 0.0f;
-    sp20.unk20 = 25.0f;
-    sp20.unk24 = D_800A1438;
+    sp20.ownerActorId = *((u8 *)arg0 + 0x3B);
+    sp20.ownerActor = arg0;
+    sp20.ownerTransformIndex = 1;
+    sp20.localOffsetX = 0.0f;
+    sp20.localOffsetY = 0.0f;
+    sp20.localOffsetZ = 0.0f;
+    sp20.spawnRadius = 25.0f;
+    sp20.spawnRate = D_800A1438;
     if (arg1 == -1) {
-        sp20.unk28 = 0x12C;
+        sp20.lifetime = 0x12C;
     } else {
-        sp20.unk28 = arg1;
+        sp20.lifetime = arg1;
     }
-    sp20.unk2A = ((arg1 == -1) ? 0 : 4) | 0xA;
-    sp20.unk2B = 7;
-    sp20.unk2C = -1;
-    sp20.unk2D = 6;
+    sp20.flags = ((arg1 == -1) ? 0 : 4) | 0xA;
+    sp20.spawnCallbackIndex = 7;
+    sp20.aliveCheckIndex = -1;
+    sp20.behaviorIndex = 6;
     return func_1513418C(&sp20, 0, 0xFF, 0);
 }
 
